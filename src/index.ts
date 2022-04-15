4882bc3a7fd08cd2b0b879de725aa6968ec365c2
Paul <denboer.paul@gmail.com>
2022-04-15 13:59:17 +0200

    Successfully imported lodash

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 // Import stylesheets
 // import 'style.css';
+import _ from 'lodash';
 
 console.log('Starting ', new Date(Date.now()).toISOString());
 
 // Write TypeScript code!
 window.onload = () => {
-    console.log('Window OnLoad');
+    const arr : string[] = _.concat([], "Hello", "World");
+    console.log(arr);
     const appDiv: HTMLElement = document.getElementById('app');
     appDiv.innerHTML = `<h2>The script worked</h2>`;
 }
