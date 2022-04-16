3ecd41e6b816469f33fc7ce81c3a67ebd587f8c8
Paul <denboer.paul@gmail.com>
2022-04-17 00:50:10 +0200

    sliding range

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,22 @@ import { NodeBuilder, NodeWrapper } from './node-builder'
 import './style.css';
 import 'core-js/actual';
 
+const minmax = function (value: number, limit1: number, limit2: number): number {
+    if (limit1 > limit2) {
+        return minmax(value, limit2, limit1);
+    }
+    if (value < limit1) return limit1;
+    if (value > limit2) return limit2;
+    return value;
+}
+
 window.onload = () => {
     const appDiv: HTMLElement = document.getElementById('app');
     appDiv.innerHTML = '';
 
     const nb = new NodeBuilder();
-    const values = [0, 50, 100];
+    const width = 500;
+    const values = [0.50*width, 0.10*width, 0.80*width];
     let thumbWrappers: NodeWrapper[] = [];
     let rangeWrapper: NodeWrapper;
     const unit = 'px';
@@ -17,7 +27,7 @@ window.onload = () => {
             nb.div({ class: 'slider-rail' }),
             // track
             rangeWrapper = nb.div({ class: 'slider-range' })
-                .style({ left: `${values[0]}${unit}`, width: `${values[1] - values[0]}${unit}` }),
+                .style({ left: `${values[1]}${unit}`, width: `${values[2] - values[1]}${unit}` }),
             // v[0]
             thumbWrappers[0] = nb.div({ class: 'slider-thumb slider-thumb-0' })
                 .style({ left: `${values[0]}${unit}` }),
@@ -28,37 +38,68 @@ window.onload = () => {
             thumbWrappers[2] = nb.div({ class: 'slider-thumb slider-thumb-2' })
                 .style({ left: `${values[2]}${unit}` })
         )
-            .style({ width: '400px' })
+            .style({ width: `${width}${unit}` })
     ).replaceTo(appDiv);
 
     const range = rangeWrapper.node;
+    let mouseDown: {target?:'range'|'thumb', clientX?:any, thumbX?:any} = {};
+    range.onpointerdown = (e: PointerEvent) => {
+        mouseDown = { target:'range', clientX: e.clientX, thumbX: (<any>e.currentTarget).offsetLeft };
+        (<any>e.currentTarget).setPointerCapture(e.pointerId);
+    };
+    range.onpointerup = (e: PointerEvent) => {
+        mouseDown = {};
+        (<any>e.currentTarget).releasePointerCapture(e.pointerId);
+    };
+    range.onpointermove = (e: PointerEvent) => {
+        if (mouseDown.target === 'range') {
+            const rangeWidth = parseInt((<string>range.style.width).replace(unit, ''));
+            const left = minmax(mouseDown.thumbX + e.clientX - mouseDown.clientX, 0, width - rangeWidth);
+            Object.assign(
+                range.style,
+                {
+                    left: `${left}${unit}`,
+                });
+            Object.assign(
+                thumbWrappers[1].node.style,
+                {
+                    left: `${left}${unit}`,
+                });
+            Object.assign(
+                thumbWrappers[2].node.style,
+                {
+                    left: `${left + rangeWidth}${unit}`,
+                });
+        }
+    };
+
     for (const thumbWrapper of thumbWrappers) {
         const thumb = thumbWrapper.node;
-        let mouseDown: {clientX?:any,thumbX?:any} = {};
         thumb.onpointerdown = (e: PointerEvent) => {
-            mouseDown = { clientX: e.clientX, thumbX: thumb.offsetLeft };
+            mouseDown = { target:'thumb', clientX: e.clientX, thumbX: thumb.offsetLeft };
             (<any>e.currentTarget).setPointerCapture(e.pointerId);
-        };
-        thumb.onpointermove = (e: PointerEvent) => {
-            if (mouseDown.clientX) {
-                Object.assign((<any>e.currentTarget).style, {
-                    left: `${mouseDown.thumbX + e.clientX - mouseDown.clientX}${unit}`
-                });
-
-                const v0 = parseInt((<string>thumbWrappers[0].node.style.left).replace(unit, ''));
-                const v1 = parseInt((<string>thumbWrappers[1].node.style.left).replace(unit, ''));
-                console.log([v0, v1]);
-                Object.assign(
-                    range.style,
-                    {
-                        left: `${v0}${unit}`,
-                        width: `${v1 - v0}${unit}`,
-                    });
-            }
         };
         thumb.onpointerup = (e: PointerEvent) => {
             mouseDown = {};
             (<any>e.currentTarget).releasePointerCapture(e.pointerId);
+        };
+        thumb.onpointermove = (e: PointerEvent) => {
+            if (mouseDown.target === 'thumb') {
+                const left = minmax(mouseDown.thumbX + e.clientX - mouseDown.clientX, 0, width);
+                Object.assign((<any>e.currentTarget).style, {
+                    left: `${left}${unit}`
+                });
+
+                const v0 = parseInt((<string>thumbWrappers[1].node.style.left).replace(unit, ''));
+                const v1 = parseInt((<string>thumbWrappers[2].node.style.left).replace(unit, ''));
+                console.log([v0, v1]);
+                Object.assign(
+                    range.style,
+                    {
+                        left: `${Math.min(v0, v1)}${unit}`,
+                        width: `${Math.abs(v1 - v0)}${unit}`,
+                    });
+            }
         };
     };
 }
